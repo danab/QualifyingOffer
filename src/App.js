af76ac29d21fe9399f088b9fbb90cf6e4e029203
Dana Bennett <danabennett10@gmail.com>
2018-11-20 17:09:57 -0500

    Don't want to rely on external URL to provide data, since this will live on in Github

--- src/App.js
+++ src/App.js
@@ -4,39 +4,34 @@ import Copy from './Copy';
 import Histogram from './Histogram';
 import Table from './Table';
 
-import axios from 'axios';
+// Data does not need to be pulled dynamically, since that's no longer the challenge...
+import data from './data.json';
 
 // This could be done on the server side...
 // Mark the top 125 players
 const sortData = data => {
 	// not strictly necessary...
-	const copy = data.slice();
+	const copy = [ ...data ];
 	copy.sort((a, b) => (a.salary < b.salary ? 1 : -1));
 	return copy.map((val, i) => {
 		return { ...val, top125: i < 125 };
 	});
 };
 
+// Manipulate data
+const sortedData = sortData(data);
+const top125data = sortedData.filter(obj => obj.top125);
+
 class App extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			loading: true
-		};
-	}
-	componentDidMount = async () => {
-		const resp = await axios.get(
-			'https://rvkaasnsr0.execute-api.us-east-1.amazonaws.com/prod'
-		);
-		const sortedData = sortData(resp.data);
-		const top125data = sortedData.filter(obj => obj.top125);
 
-		this.setState({
+		this.state = {
 			loading: false,
 			sortedData,
 			top125data
-		});
-	};
+		};
+	}
 	render() {
 		if (this.state.loading) {
 			return <Loading />;
